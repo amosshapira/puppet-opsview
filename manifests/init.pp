697e3b016e99536ca1276c225eb2c6d37ef18d0b
Amos Shapira <ashapira@atlassian.com>
2013-02-06 15:53:56 +1100

    install required packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,26 @@
 class opsview($url, $username, $password)
 {
   file { '/etc/puppet/opsview.conf':
-    ensure  => present,
+    ensure  => 'present',
     content => template('opsview/etc/puppet/opsview.conf.erb'),
     mode    => '0644', # TODO: consider stricter permissions
+  }
+
+  case $::osfamily {
+    'RedHat' : {
+      $packages = [ Package['rubygems'], Package['ruby-devel'], Package['gcc'], Package['make'],]
+    }
+    'Debian' : {
+      $packages = [ Package['rubygems'], Package['ruby-dev'], Package['gcc'], Package['make'], ]
+    }
+    default : {
+      fail("Unsupported osfamily: \"${::osfamily}\"")
+    }
+  }
+
+  package { ['rest-client', 'json']:
+    ensure   => 'present',
+    provider => 'gem',
+    require  => $packages
   }
 }
